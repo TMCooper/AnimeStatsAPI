6756f695512f44f7489c723617c05b8a2c29a647
TMCooper <91787869+TMCooper@users.noreply.github.com>
2025-11-24 15:53:45 +0100

    Update api.py

--- src/api.py
+++ src/api.py
@@ -16,6 +16,9 @@ class Yui:
         saison_num = request.args.get("s", "").strip()
         num_episode = request.args.get("e", "").strip()
 
+        if not nom_anime or saison_num or num_episode:
+            return jsonify({"satus": "args manquans", "saved": False})
+
         Cardinal.statCollect(nom_anime, saison_num, num_episode)
 
         return jsonify({"status": "ok", "saved": True})
